64ec1b7ff1fd2aeab9f5c042fe332aa5516ea8d2
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2023-06-14 18:44:07 +0100

    added event upload strategy added smooth scroll effects added support for mdx for event info updated event overview page added backtotop button fucntionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,40 @@ import Events from "@/components/LandingPage/Events";
 import HostEvent from "@/components/LandingPage/HostEventCta";
 import Partners from "@/components/LandingPage/Partners";
 import MainLayout from "@/components/Layouts/MainLayout/MainLayout";
-import SEO from "@/components/Seo";
+import { POSTS_PATH, postFilePaths } from "@/common/mdxUtils";
+import path from "path";
+import fs from "fs";
+import matter from "gray-matter";
 
-export default function Home() {
+export default function Home({ events }) {
   return (
     <>
       <Hero />
       <About />
-      <Events />
+      <Events events={events} />
       <Partners />
       <Donate />
       <HostEvent />
     </>
   );
 }
+
+export const getStaticProps = async () => {
+  //read events from mdx files
+  const events = postFilePaths.map((file_path) => {
+    const postFilePath = path.join(POSTS_PATH, file_path);
+    const source = fs.readFileSync(postFilePath);
+    const { data } = matter(source);
+    return { ...data, page_url: file_path?.replace(/\.mdx?$/, "") };
+  });
+
+  return {
+    props: {
+      events: JSON.parse(JSON.stringify(events))?.sort(
+        (a, b) => new Date(b.eventDate) - new Date(a.eventDate)
+      ),
+    },
+  };
+};
 
 Home.layout = MainLayout;
